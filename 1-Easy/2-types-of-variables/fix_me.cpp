0dc8240f2b91d7501ab5769748789c78fca4aee5
Thealexio-exe <Alessio.argenti@peano.edu.it>
2026-09-18 16:00:43 +0200

    v4.5: add code in 2/fix_me

--- 1-Easy/2-types-of-variables/fix_me.cpp
+++ 1-Easy/2-types-of-variables/fix_me.cpp
@@ -1,1 +1,19 @@
-// Nothing to see here; I still need to work on this file.
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+// All the errors are in the variables, good luck :)
+int main() {
+
+    int var_int = -100;
+
+    float var_float = 3.145;
+    double var_double = 2,71828;
+    
+    bool var_bool = None;
+
+    char var_char = "A";
+    string var_string = 'Hi world';  
+
+}
